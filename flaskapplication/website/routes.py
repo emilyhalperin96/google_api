c87d25dd42996216b5d4461a666a4d5cb8f7a49a
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-12 15:27:47 -0400

    finished api set up

--- flaskapplication/website/routes.py
+++ flaskapplication/website/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask import request
+from .models import Result 
 
 routes = Blueprint('routes', __name__)
 
@@ -13,5 +14,14 @@ def home():
     start = 1 
     url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
     data = request.get(url).json()
-    return data 
-
+    #convert JSON response to an array of Result objections and return that array 
+    search_items = data.get('items')
+    results = []
+    for i, search_item in enumerate(search_items, start=1):
+        try:
+            long_description = search_item["pagemap"]["metatags"][0]["og:description"]
+        except KeyError:
+            long_description = "N/A"
+        print(long_description)
+        results.append(Result(title=search_item.get("title"),description=long_description, snippet=search_item.get("snippet"),url=search_item.get("link")))
+    return render_template('response_view.html', results=results)
