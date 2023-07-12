a089c6c3bbc28e839e8d2a987a93a77eb88c2e57
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-12 12:56:52 -0400

    created app and db

---
+++ flaskapplication/website/routes.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+from flask import request
+
+routes = Blueprint('routes', __name__)
+
+@routes.route('/')
+def home():
+    return "Welcome to Educative!"
